dffaea597db9db10ecd81babf75ec1c57adcd5e7
Bogdawaa <32144666+Bogdawaa@users.noreply.github.com>
2020-10-07 19:22:10 +0300

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Bogdan on 07.10.2020.
+//  Copyright © 2020 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
